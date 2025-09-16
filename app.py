446ad6b7a50d0ea5e0640b62e1f937808aa87530
GustavoAftonAgatsuma <161485851+GustavoAftonAgatsuma@users.noreply.github.com>
2025-09-16 08:05:17 -0300

    aula1

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
+# rodar no cmd
+#pip install flask
 
+#importar a bliblioteca flask
+from flask import flask
+#criar uma instancia da aplicação flask
+app = flask(__name__)
+#este é um decorador que assosia a URL
+# '/' (a URL raiz do site) à função que vem logo abaixo
+@app.route('/")
+#a função que é executada quando a rota '/' é acessada
+#ela retorna a string "Hello, World!""
+def hello_world()
+    return 'Hello, World!'
+
+#executa o servidor de desenvolvimento
+if __name__ == '__main__':
+    app.run(debug=True)
